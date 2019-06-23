09a4e9b64b6affb316ff1f3f2b1d9c5466d84016
Topher Hunt <hunt.topher@gmail.com>
2019-06-23 11:53:47 +0200

    Add Heroku buildpack config

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EyeTest.MixProject do
     [
       app: :eye_test,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
